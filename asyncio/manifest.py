1b623dac2a90b985aa7aecd5d67e8e0fad3fa7f2
Dan Halbert <halbert@halwitz.org>
2021-11-10 10:47:13 -0500

    add SPDX licensing

--- asyncio/manifest.py
+++ asyncio/manifest.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2019 Damien P. George
+#
+# SPDX-License-Identifier: MIT
+#
 # This list of frozen files doesn't include task.py because that's provided by the C module.
 freeze(
     "..",
