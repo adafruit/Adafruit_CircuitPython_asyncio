33949c60c6f22499693ce93d9e8cdfda92e227dc
SquirtleSquadLeader <112285567+SquirtleSquadLeader@users.noreply.github.com>
2023-11-27 21:00:13 -0500

    Update asyncio_simpletest.py
    Contributing an example.  My hope is that this code will let someone quickly set up an asyncio project on any mcu.

--- examples/asyncio_simpletest.py
+++ examples/asyncio_simpletest.py
@@ -1,4 +1,45 @@
 # SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
 # SPDX-FileCopyrightText: Copyright (c) 2021 Dan Halbert for Adafruit Industries
-#
 # SPDX-License-Identifier: Unlicense
+# Contributor: Sean Carney
+
+"""
+Example that illustrates how to use asyncio to execute two functions at 
+user-defined time intervals.  
+"""
+
+# Imports
+import asyncio
+from adafruit_ticks import ticks_ms, ticks_add, ticks_less, ticks_diff
+
+async def function_1():
+    print('Execturing function_1')
+    await asyncio.core.sleep(0)
+    
+async def function_2(led):
+    print('Execturing function_2')
+    await asyncio.core.sleep(0)
+
+async def main():
+
+    # Desired time delays in milliseconds
+    delay_1 = 1000
+    delay_2 = 100
+    
+    # Set start of frame
+    start_function_1 = ticks_ms()
+    start_function_2 = ticks_ms()    
+        
+    while True:    
+      
+        if ticks_diff(ticks_ms(), start_function_1 ) > delay_1:
+            start_function_1 = ticks_ms()
+            asyncio.core.create_task(function_1())
+            
+        if ticks_diff(ticks_ms(), start_function_2) > delay_2:
+            start_function_2 = ticks_ms()
+            asyncio.core.create_task(function_2())
+        
+        await asyncio.core.sleep(0)
+        
+asyncio.core.run(main(led)) 
